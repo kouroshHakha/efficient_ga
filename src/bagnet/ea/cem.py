1. refactored ea 2. fixed bug in CEM: fix the variance instead of computing it

src/bagnet/ea/cem.py
@@ -1,16 +1,17 @@
 from scipy.stats import multivariate_normal
 from scipy.stats import gaussian_kde
 import numpy as np
-from .ea import EA
-from util import Design
 
+from bb_eval_engine.data.design import Design
+
+from .ea import EA
 
 class CEM(EA):
     """
     The vanilla implementation of Cross Entropy method with gaussian underlying distribution
     """
     def __init__(self, eval_core, n, dist_type='gaussian'):
-        self.eval_core = eval_core
+        EA.__init__(self, eval_core)
         dim = len(self.eval_core.params)
         self.n_new_samples = n
         self.type = dist_type
@@ -23,7 +24,8 @@ class CEM(EA):
     def fit(self, data):
         if self.type == 'gaussian':
             self.mu = np.mean(data, axis=0)
-            self.var = np.var(data, axis=0)
+            self.var = np.eye(data.shape[-1]) * 10
+            # self.var = np.var(data, axis=0)
             # print("[debug] cem variance = {}".format(self.var))
         elif self.type == 'kde':
             self.kde = gaussian_kde(np.transpose(data))
@@ -43,13 +45,11 @@ class CEM(EA):
         if offsprings.ndim == 1:
             offsprings = offsprings[None, :]
 
-        dsns = list()
+        dsns = []
         for dsn in offsprings:
             dsn = np.clip(dsn, self.eval_core.params_min, self.eval_core.params_max)
             dsn = np.floor(dsn).astype(int)
-            dsn = Design(self.eval_core.spec_range,
-                         self.eval_core.id_encoder,
-                         dsn.tolist())
+            dsn = Design(dsn.tolist())
             dsns.append(dsn)
 
         return dsns