049d6e4bb47b78fbfd84aa70a927d33f7214d45c
Kourosh Hakhamaneshi <kourosh_hakhamaneshi@berkeley.edu>
2020-02-09 16:39:56 -0800

    refactored the bagnet agent

--- src/bagnet/agent/bagnet.py
+++ src/bagnet/agent/bagnet.py
@@ -1,11 +1,13 @@
 """
 This module implements the BagNet algorithm
 """
-import importlib
+from typing import cast, Type, List
 import time
 import os.path as osp
 
+from utils.importlib import import_class
 from .base import Agent
+from ..model.model import Model
 
 
 class BagNetAgent(Agent):
@@ -13,27 +15,25 @@ class BagNetAgent(Agent):
     def __init__(self, fname):
         Agent.__init__(self, fname)
 
-        model_module = importlib.import_module(self.specs['model_module_name'])
-        model_cls = getattr(model_module, self.specs['model_class_name'])
-        self.model = model_cls(num_params_per_design=self.eval_core.num_params,
-                               spec_kwrd_list=list(self.eval_core.spec_range.keys()),
-                               logger=self.logger,
+        model_cls = cast(Type[Model], import_class(self.specs['model_cls']))
+        self.model = model_cls(num_params_per_design=len(self.bb_env.params),
+                               spec_kwrd_list=list(self.bb_env.spec_range.keys()),
+                               logger=self._logger,
                                **self.specs['model_params'])
 
-        # get the parameters in agent_params that are related to deep learning
-        self.valid_frac = self.agent_params['valid_frac']
-        self.max_n_retraining = self.agent_params['max_n_retraining']
-
+        # get the parameters in specs that are related to the model
+        self.valid_frac = self.specs['valid_frac']
+        self.max_n_retraining = self.specs['max_n_steps']
         # from db designs with indices less than this number are considered during training
-        self.k = self.agent_params['k']
-
+        self.k = self.specs['k']
         # training settings
-        self.num_epochs = self.agent_params['num_epochs']
-        self.batch_size = self.agent_params['batch_size']
-        self.display_step = self.agent_params['display_step']
-        self.ckpt_step = self.agent_params['ckpt_step']
-        self.max_iter = self.agent_params['max_iter']
+        self.ngrad_update_per_iter = self.specs.get('ngrad_update_per_iter', 1000)
+        self.batch_size = self.specs['batch_size']
+        self.display_step = self.specs['display_step']
+        self.ckpt_step = self.specs['ckpt_step']
+        self.max_iter = self.specs['max_iter']
 
+        self.n_new_samples = self.specs['n_new_samples']
         # paper stuff variables
         self.n_sims = 0
         self.sim_time = 0
@@ -48,27 +48,27 @@ class BagNetAgent(Agent):
 
     def train(self):
         t_minus = time.time()
-        ds = self.model.get_train_valid_ds(self.db, self.k, self.eval_core, self.valid_frac)
-        self.model.train(ds, self.batch_size, self.num_epochs, self.ckpt_step, self.display_step)
+        ds = self.model.get_train_valid_ds(self.db, self.k, self.bb_env, self.valid_frac)
+        self.model.train(ds, self.batch_size, self.ngrad_update_per_iter, self.ckpt_step, self.display_step)
         t_plus = time.time()
         self.n_training += 1
         self.training_time += (t_plus - t_minus)
-        self.logger.log_text("[INFO] training done %.2fSec" % (t_plus - t_minus))
+        self.info("Training done %.2fSec" % (t_plus - t_minus))
 
-    def run(self):
-
-        self.decision_box.update_heuristics(self.db, self.k)
+    def run_per_iter(self):
+        db_list = list(self.db)
+        self.decision_box.update_heuristics(db_list, self.k)
 
         if self.decision_box.has_converged():
             return [], True
 
-        parent1, parent2, ref_design = self.decision_box.get_parents_and_ref_design(self.db, self.k)
+        parent1, parent2, ref_design = self.decision_box.get_parents_and_ref_design(db_list, self.k)
 
         offsprings = []
         n_iter = 0
 
-        self.logger.log_text(30*"-")
-        self.logger.log_text("[INFO] running model ... ")
+        self.log(30*"-")
+        self.info("Running model ... ")
         q_start = time.time()
 
         while_time = 0
@@ -78,7 +78,7 @@ class BagNetAgent(Agent):
         decision_time = 0
         deletion_time = 0
 
-        self.ea.prepare_for_generation(self.db, self.k)
+        self.ea.prepare_for_generation(db_list, self.k)
         while_s = time.time()
         while len(offsprings) < self.n_new_samples and n_iter < self.max_iter:
             gen_s = time.time()
@@ -88,10 +88,9 @@ class BagNetAgent(Agent):
 
             for new_design in new_designs:
                 check_s = time.time()
-                if any([(new_design == row) for row in self.db]) or \
-                        any([(new_design == row) for row in offsprings]):
+                if new_design in self.db or new_design in offsprings:
                     # if design is already in the design pool skip ...
-                    self.logger.log_text("[debug] design {} already exists".format(new_design))
+                    self.debug(f"Design {new_design} already exists")
                     continue
                 check_e = time.time()
                 check_time += check_e - check_s
@@ -112,55 +111,43 @@ class BagNetAgent(Agent):
                 if is_new_design_better:
                     offsprings.append(new_design)
                 deletion_e = time.time()
-                deletion_time += decision_e - deletion_s
+                deletion_time += deletion_e - deletion_s
 
         while_e = time.time()
         while_time = while_e - while_s
         self.query_time += time.time() - q_start
 
-        self.logger.log_text("avg_gen_time = {}".format(gen_time/n_iter))
-        self.logger.log_text("avg_check_time = {}".format(check_time/n_iter))
-        self.logger.log_text("avg_q_time = {}".format(q_time/n_iter))
-        self.logger.log_text("avg_decision_time = {}".format(decision_time/n_iter))
-        self.logger.log_text("avg_deletion_time = {}".format(deletion_time/n_iter))
-        self.logger.log_text("avg_while_time = {}".format(while_time/n_iter))
+        self.info("Avg_gen_time = {}".format(gen_time/n_iter))
+        self.info("Avg_check_time = {}".format(check_time/n_iter))
+        self.info("Avg_q_time = {}".format(q_time/n_iter))
+        self.info("Avg_decision_time = {}".format(decision_time/n_iter))
+        self.info("Avg_deletion_time = {}".format(deletion_time/n_iter))
+        self.info("Avg_while_time = {}".format(while_time/n_iter))
 
         if len(offsprings) < self.n_new_samples:
             return offsprings, True
 
-        self.logger.log_text(30*"-")
-        for offspring in offsprings:
-            self.logger.log_text(offspring)
-
+        self.log(30*"-")
         s = time.time()
-        design_results = self.eval_core.evaluate(offsprings)
+        offsprings = cast(List, self.bb_env.evaluate(offsprings))
         e = time.time()
         self.n_sims += len(offsprings)
         self.sim_time += e - s
 
-        self.logger.log_text('[INFO] design evaluation time: {:.2f}'.format(e-s))
+        self.info('Design evaluation time: {:.2f}'.format(e-s))
         list_to_be_removed = []
-        for i, design in enumerate(offsprings):
-            design_result = design_results[i]
-            if design_result['valid']:
-                design.cost = design_result['cost']
-                for key in design.specs.keys():
-                    design.specs[key] = design_result[key]
-
-                self.logger.log_text("[debug] design {} with cost {} was added".format(design,
-                                                                                       design.cost))
-                self.logger.log_text("[debug] {}".format(design.specs))
-
+        for child in offsprings:
+            if not child['valid']:
+                list_to_be_removed.append(child)
+                self.debug(f"Design {child} did not produce valid results")
             else:
-                self.logger.log_text("[debug] design {} did not produce valid results".format(
-                    design))
-                list_to_be_removed.append(design)
+                self.info(f"Added: {child} , cost = {child['cost']}")
 
         for design in list_to_be_removed:
             offsprings.remove(design)
 
-        self.logger.log_text("[INFO] new designs tried: %d" % n_iter)
-        self.logger.log_text("[INFO] new candidates size: %d " % len(offsprings))
+        self.info(f"New designs tried: {n_iter}")
+        self.info(f"New candidates size: {len(offsprings)} ")
 
         return offsprings, False
 
@@ -168,63 +155,61 @@ class BagNetAgent(Agent):
         start = time.time()
 
         self.get_init_population()
-        self.data_set_list.append(self.db)
+        self.data_set_list.append(list(self.db))
         self.update_time_info(0)
 
         self.model.init()
         self.train()
 
         for i in range(self.max_n_retraining):
-
-            offsprings, is_converged = self.run()
+            self.info(f'********** Iter {i} **********')
+            offsprings, is_converged = self.run_per_iter()
 
             if is_converged:
                 break
             elif len(offsprings) == 0:
                 continue
 
-            self.db = self.db + offsprings
+            self.db.extend(offsprings)
             self.data_set_list.append(offsprings)
-            self.logger.store_db(self.data_set_list)
+            self._logger.store_db(self.data_set_list)
             self.update_time_info(time.time()-start)
             self.store_database_and_times()
 
             # adjust dataset size for training, if not desired, comment the agent.k_top= ... line
-            self.db = sorted(self.db, key=lambda x: x.cost)
-            worst_offspring = max(offsprings, key=lambda x: x.cost)
-            self.logger.log_text('[INFO] k_top alternative: {}'.format(self.db.index(
-                worst_offspring)))
-            self.k = max(self.n_init_samples, self.db.index(worst_offspring))
+            db_sorted = sorted(self.db, key=lambda x: x['cost'])
+            worst_offspring = max(offsprings, key=lambda x: x['cost'])
+            self.info(f'k_top alternative: {db_sorted.index(worst_offspring)}')
+            # self.k = max(self.n_init_samples, db_sorted.index(worst_offspring))
 
             self.train()
-            self.logger.log_text("[INFO] n_queries = {}".format(self.n_queries))
-            self.logger.log_text("[INFO] query_time = {}".format(self.query_time))
-            self.logger.log_text("[INFO] n_simulations = {}".format(self.n_sims))
-            self.logger.log_text("[INFO] sim_time = {}".format(self.sim_time))
-            self.logger.log_text("[INFO] n_training = {}".format(self.n_training))
-            self.logger.log_text("[INFO] training_time = {}".format(self.training_time))
-            self.logger.log_text("[INFO] total_time = {}".format(time.time()-start))
+            self.info(f"Nqueries = {self.n_queries}")
+            self.info(f"Query_time = {self.query_time}")
+            self.info(f"Total_time = {time.time()-start}")
+            self.info(f"sim_time = {self.sim_time}")
+            self.info(f"n_training = {self.n_training}")
+            self.info(f"training_time = {self.training_time}")
+            self.info(f"total_time = {time.time() - start}")
 
-        self.logger.store_db(self.data_set_list)
+        self._logger.store_db(self.data_set_list)
         self.store_database_and_times()
 
-        sorted_db = sorted(self.db, key=lambda x: x.cost)
+        sorted_db = sorted(self.db, key=lambda x: x['cost'])
         # paper stuff
-        self.logger.log_text("[finished] n_queries = {}".format(self.n_queries))
-        self.logger.log_text("[finished] query_time = {}".format(self.query_time))
-        self.logger.log_text("[finished] n_simulations = {}".format(self.n_sims))
-        self.logger.log_text("[finished] sim_time = {}".format(self.sim_time))
-        self.logger.log_text("[finished] n_training = {}".format(self.n_training))
-        self.logger.log_text("[finished] training_time = {}".format(self.training_time))
-        self.logger.log_text("[finished] total_time = {}".format(time.time()-start))
-
-        self.logger.log_text("[finished] total_n_evals = {}".format(len(self.db)))
-        self.logger.log_text("[finished] best_solution = {}".format(sorted_db[0]))
-        self.logger.log_text("[finished] id = {}".format(sorted_db[0].id))
-        self.logger.log_text("[finished] cost = {}".format(sorted_db[0].cost))
-        self.logger.log_text("[finished] performance \n{} ".format(sorted_db[0].specs))
+        self.info(f"n_queries = {self.n_queries}")
+        self.info(f"query_time = {self.query_time}")
+        self.info(f"n_simulations = {self.n_sims}")
+        self.info(f"sim_time = {self.sim_time}")
+        self.info(f"n_training = {self.n_training}")
+        self.info(f"training_time = {self.training_time}")
+        self.info(f"total_time = {time.time() - start}")
+        self.info(f"total_n_evals = {len(self.db)}")
+        self.info(f"best_solution = {sorted_db[0]}")
+        self.info(f"id = {sorted_db[0]['id']}")
+        self.info(f"cost = {sorted_db[0]['cost']}")
+        self.info(f"Performance \n{sorted_db[0].specs} ")
         for ind in sorted_db[:self.decision_box.ref_index]:
-            self.logger.log_text("{} -> {} -> {}".format(ind, ind.cost, ind.specs))
+            self.log(f"{ind}, cost = {ind['cost']}")
 
     def update_time_info(self, total_time):
         self.n_sims_list.append(self.n_sims)
@@ -246,4 +231,4 @@ class BagNetAgent(Agent):
             training_time=self.training_time_list,
             total_time=self.total_time_list,
         )
-        self.logger.store_db(dict_to_save, fpath=osp.join(self.logger.log_path, 'db_time.pkl'))
+        self._logger.store_db(dict_to_save, fpath=osp.join(self._logger.log_path, 'db_time.pkl'))
